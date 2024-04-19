c139c1c9b2b8795cdc97a1980a816b035d42496f
Bao Anh <nguyenlebaoanh05@gmail.com>
2024-04-19 16:28:54 -0400

    cookie

--- script.js
+++ script.js
@@ -12,23 +12,25 @@ function setDarkMode() {
 const cookieBox = document.querySelector(".wrapper"),
   buttons = document.querySelectorAll(".button");
 
-const executeCodes = () => {
-  //if cookie contains coding it will be returned and below of this code will not run
-  if (document.cookie.includes("LaSalle_Coding")) return;
-  cookieBox.classList.add("show");
-
-  buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-      cookieBox.classList.remove("show");
-
-      //if button has acceptBtn id
-      if (button.id == "acceptBtn") {
-        //set cookies for 1 month. 60 = 1 min, 60 = 1 hours, 24 = 1 day, 30 = 30 days
-        document.cookie = "cookieBy= LaSalle_Coding; max-age=" + 60 * 60 * 24 * 30;
-      }
+  const executeCodes = () => {
+    //if cookie contains coding it will be returned and below of this code will not run
+    if (document.cookie.includes("cookieBy=LaSalle_Coding")) return;
+    cookieBox.classList.add("show");
+  
+    buttons.forEach((button) => {
+      button.addEventListener("click", () => {
+        cookieBox.classList.remove("show");
+  
+        //if button has acceptBtn id
+        if (button.id == "acceptBtn") {
+          //set cookies for 1 month. 60 = 1 min, 60 = 1 hours, 24 = 1 day, 30 = 30 days
+          document.cookie = "cookieBy=LaSalle_Coding; max-age=" + 60 * 60 * 24 * 30;
+        }
+      });
     });
-  });
-};
+  };
+  window.addEventListener("load", executeCodes);
+
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
       cookieBox.style.display = 'none';
@@ -37,7 +39,7 @@ buttons.forEach((button) => {
       }, 500);
       if (button.id == "acceptBtn") {
           // Set cookies for 1 month. 60 = 1 min, 60 = 1 hours, 24 = 1 day, 30 = 30 days
-          document.cookie = "cookieBy= LaSalle_Coding; max-age=" + 60 * 60 * 24 * 30;
+          document.cookie = "cookieBy=LaSalle_Coding; max-age=" + 60 * 60 * 24 * 30;
       }
   });
 });
@@ -53,9 +55,6 @@ document.addEventListener('DOMContentLoaded', function() {
       document.getElementById('rememberMeCheckbox').style.display = 'none';
   });
 });
-
-window.addEventListener("load", executeCodes);
-
 function setCookie(name, value, days) {
   var expires = "";
   if (days) {
